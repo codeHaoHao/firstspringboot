b80974a51d1d5c26d9dd5155c3d6444eac1202c2
franky <franky@dynadot.com>
2019-08-28 11:33:06 +0800

    Unified management of common css

---
+++ src/main/resources/static/js/basic-js.js
@@ -0,0 +1,2 @@
+document.write("<script type='text/jacascript' scr='jquery/jquery-3.4.1.min.js'></script>");
+document.write("<script type='text/jacascript' scr='bootstrap/bootstrap.min.js'></script>");
