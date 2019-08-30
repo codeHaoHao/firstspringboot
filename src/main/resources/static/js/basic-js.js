e6bdf0d546c6ac9b870643a2a58dc08ba5092ac5
franky <franky@dynadot.com>
2019-08-30 15:50:17 +0800

    Change the reference method of the js file to the reference way of js in ES6

--- src/main/resources/static/js/basic-js.js
+++
@@ -1,2 +0,0 @@
-document.write("<script type='text/jacascript' scr='jquery/jquery-3.4.1.min.js'></script>");
-document.write("<script type='text/jacascript' scr='bootstrap/bootstrap.min.js'></script>");
---
+++ src/main/resources/static/js/commonJs.js
@@ -0,0 +1,3 @@
+import '/static/js/jquery/jquery-3.4.1.min.js';
+import '/static/js/bootstrap/bootstrap.min.js';
+import '/static/js/vue/vue.min.js';
