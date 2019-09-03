13776a1f7cb5859952986c578c4544765e5011ae
franky <franky@dynadot.com>
2019-09-03 10:32:43 +0800

    改变springmvc自定义请求的拦截方式

--- src/main/java/cn/lijiahao/interceptor/BaseInterceptor.java
+++ src/main/java/cn/lijiahao/interceptor/BaseInterceptor.java
@@ -12,15 +12,19 @@ public class BaseInterceptor implements HandlerInterceptor{
 	@Autowired
 	private StringRedisTemplate stringRedisTemplate;
 	
+	
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		String sessionId = request.getSession().getId();
+		System.out.println(sessionId);
 		if (stringRedisTemplate.opsForHash().get(sessionId, "sessionId") != null
 				&& stringRedisTemplate.opsForHash().get(sessionId, "sessionId").equals(sessionId)) {
-			response.sendRedirect("/sigin");
+			
 			return true;
 		}else {
+			System.out.println("contextPath:"+request.getContextPath());
 			request.getRequestDispatcher(request.getContextPath()+"/sigin").forward(request, response);
 			
 			return true;
--- src/main/java/cn/lijiahao/interceptor/SpringMVCConfig.java
+++ src/main/java/cn/lijiahao/interceptor/SpringMVCConfig.java
@@ -1,7 +1,12 @@
 package cn.lijiahao.interceptor;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -10,13 +15,41 @@ public class SpringMVCConfig extends WebMvcConfigurationSupport{
 	@Autowired
 	private BaseInterceptor baseInterceptor;
 	
+	private List<String> excluPathPatterns;//不被拦截的请求
+	
+	
 	@Override
 	protected void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(baseInterceptor).addPathPatterns("/*").excludePathPatterns("/static/**","/sigin");
+		initExcludePathPatterns();
+		registry.addInterceptor(baseInterceptor).addPathPatterns("/**").excludePathPatterns(excluPathPatterns);
 	}
 	
 	@Override
 	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/**");
+		registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/static/css/favicon.ico");
+	}
+	/**
+	 * 初始化不被拦截的请求
+	 */
+	private void initExcludePathPatterns() {
+		try {
+			excluPathPatterns = FileUtils.readLines(ResourceUtils.getFile("classpath:springmvc-interceptor.properties"), "utf-8");
+			initList();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	private void initList() {
+		for(int i=0;i<excluPathPatterns.size();i++) {
+			if(excluPathPatterns.get(i).startsWith("###")) {
+				excluPathPatterns.remove(i);
+			}
+			if(excluPathPatterns.get(i).equals("excluPathPatterns:")) {
+				excluPathPatterns.remove(i);
+			}
+		}
 	}
 }
