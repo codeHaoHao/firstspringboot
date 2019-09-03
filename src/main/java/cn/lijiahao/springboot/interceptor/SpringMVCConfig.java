fe683df9928d28917cb1f1afa507c0a8e3437028
franky <franky@dynadot.com>
2019-09-03 15:23:23 +0800

    111

--- src/main/java/cn/lijiahao/springboot/interceptor/SpringMVCConfig.java
+++ src/main/java/cn/lijiahao/springboot/interceptor/SpringMVCConfig.java
@@ -1,4 +1,4 @@
-package cn.lijiahao.firstspringboot.interceptor;
+package cn.lijiahao.springboot.interceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
