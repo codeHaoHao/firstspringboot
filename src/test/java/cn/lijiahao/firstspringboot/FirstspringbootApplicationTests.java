3bcca0f1ea15721db8bd585ffdb83738a8ffb047
franky <franky@dynadot.com>
2019-09-19 10:07:25 +0800

    add test

--- src/test/java/cn/lijiahao/firstspringboot/FirstspringbootApplicationTests.java
+++ src/test/java/cn/lijiahao/firstspringboot/FirstspringbootApplicationTests.java
@@ -5,13 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import cn.lijiahao.md5.Md5Utils;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FirstspringbootApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		
+		System.out.println("1024  MD5:   "+Md5Utils.encrypt("1024", "1024"));
 	}
 
 }
